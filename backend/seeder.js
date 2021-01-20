3a859f89ed2ba0fd21ffd99fc028b672dbbe23bc
vhalvarez <alvarez.programmer@gmail.com>
2021-01-20 13:02:54 -0400

    Creamos un script para almacenar datos por defecto

---
+++ backend/seeder.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const User = require('./models/userModel')
+const Product = require('./models/productModel')
+const Order = require('./models/orderModel')
+
+// data de prueba
+const users = require('./data/user')
+const products = require('./data/products')
+
+const { connectDB } = require('./config/db')
+
+dotenv.config()
+connectDB()
+
+const importData = async () => {
+    try {
+        await Order.deleteMany()
+        await Product.deleteMany()
+        await User.deleteMany()
+
+        const createdUsers = await User.insertMany(users)
+        const adminUser = createdUsers[0]._id
+
+        const sampleProducts = products.map((product) => {
+            return { ...product, user: adminUser }
+        })
+
+        await Product.insertMany(sampleProducts)
+
+        console.log('Data imported!'.green.inverse)
+        process.exit()
+    } catch (error) {
+        console.error(`${error}`.red.inverse)
+        process.exit(1)
+    }
+}
+
+const destroyData = async () => {
+    try {
+        await Order.deleteMany()
+        await Product.deleteMany()
+        await User.deleteMany()
+
+        console.log('Data destroy!'.red.inverse)
+        process.exit()
+    } catch (error) {
+        console.error(`${error}`.red.inverse)
+        process.exit(1)
+    }
+}
+
+if (process.argv[2] === '-d') {
+    destroyData()
+} else {
+    importData()
+}
