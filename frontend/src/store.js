ceb2789ab5ed772617f6cc79fe9d370820c742b2
vhalvarez <alvarez.programmer@gmail.com>
2021-01-22 12:21:17 -0400

    REDUX: Creamos el reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+    productList: productListReducer
+})
+
 const initialState = {}
+
 const middleware = [thunk]
+
 const store = createStore(
     reducer,
     initialState,
