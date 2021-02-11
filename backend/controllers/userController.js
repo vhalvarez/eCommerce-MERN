5e9ac01843ebef0d50e7a200182fec122b4fffba
vhalvarez <alvarez.programmer@gmail.com>
2021-02-11 12:13:07 -0400

    Crear controlador para actualizar el perfil

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,7 +25,7 @@ const authUser = asyncHandler(async (req, res) => {
 })
 
 // @desc    Register a new user
-// @route   PPOST /api/users
+// @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
@@ -76,4 +76,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = { authUser, registerUser, getUserProfile }
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+
+        if(req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updateUser = await user.save()
+
+        res.json({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email,
+            isAdmin: updateUser.isAdmin,
+            token: generateToken(updateUser._id),
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+module.exports = { authUser, registerUser, getUserProfile,  updateUserProfile }
