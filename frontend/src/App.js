89d705fd9fddb41cb61ee37708a9cc037d641277
vhalvarez <alvarez.programmer@gmail.com>
2021-02-17 19:25:01 -0400

    Payment screen & save Payment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
 
 const App = () => {
     return (
@@ -18,30 +19,13 @@ const App = () => {
             <main className='py-3'>
                 <Container>
                     <Route path='/' component={HomeScreen} exact />
-                    <Route
-                        path='/product/:id'
-                        component={ProductScreen}
-                    />
-                    <Route
-                        path='/login'
-                        component={LoginScreen}
-                    />
-                    <Route
-                        path='/shipping'
-                        component={ShippingScreen}
-                    />
-                    <Route
-                        path='/register'
-                        component={RegisterScreen}
-                    />
-                    <Route
-                        path='/profile'
-                        component={ProfileScreen}
-                    />
-                    <Route
-                        path='/cart/:id?'
-                        component={CartScreen}
-                    />
+                    <Route path='/product/:id' component={ProductScreen} />
+                    <Route path='/login' component={LoginScreen} />
+                    <Route path='/shipping' component={ShippingScreen} />
+                    <Route path='/payment' component={PaymentScreen} />
+                    <Route path='/register' component={RegisterScreen} />
+                    <Route path='/profile' component={ProfileScreen} />
+                    <Route path='/cart/:id?' component={CartScreen} />
                 </Container>
             </main>
             <Footer />
