0b0baeab011fb67bb51ed7a9aa2322e9533c59e6
vhalvarez <alvarez.programmer@gmail.com>
2021-02-24 12:15:17 -0400

    Get order by id endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const {
-    addOrderItems
+    addOrderItems,
+    getOrderById,
 } = require('../controllers/orderController')
 const { protect } = require('../middlewares/authMiddleware')
 
-
 router.route('/').post(protect, addOrderItems)
+router.route('/:id').get(protect, getOrderById)
 
 module.exports = router
