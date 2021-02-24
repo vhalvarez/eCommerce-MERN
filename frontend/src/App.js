621d55f6f3e7aae43dce5ed0c96ce156fde13c59
vhalvarez <alvarez.programmer@gmail.com>
2021-02-24 13:15:22 -0400

    Order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
 
 const App = () => {
     return (
@@ -21,10 +22,12 @@ const App = () => {
                 <Container>
                     <Route path='/' component={HomeScreen} exact />
                     <Route path='/product/:id' component={ProductScreen} />
+                    
                     <Route path='/login' component={LoginScreen} />
                     <Route path='/shipping' component={ShippingScreen} />
                     <Route path='/payment' component={PaymentScreen} />
                     <Route path='/placeorder' component={PlaceOrderScreen} />
+                    <Route path='/order/:id' component={OrderScreen} />
                     <Route path='/register' component={RegisterScreen} />
                     <Route path='/profile' component={ProfileScreen} />
                     <Route path='/cart/:id?' component={CartScreen} />
