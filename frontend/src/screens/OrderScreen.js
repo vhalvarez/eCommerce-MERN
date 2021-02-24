24bef80d1e66903107036be34613052b4a37f29e
vhalvarez <alvarez.programmer@gmail.com>
2021-02-24 13:28:28 -0400

    Update to paid endpoint

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -28,8 +28,10 @@ const OrderScreen = ({ match }) => {
     }
 
     useEffect(() => {
-        dispatch(getOrderDetails(orderId))
-    }, [])
+        if(!order || order._id !== orderId) {
+            dispatch(getOrderDetails(orderId))
+        }
+    }, [dispatch, order, orderId]) 
 
     return loading ? (
         <Loader />
