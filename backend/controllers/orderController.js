75446a04a1f9eabbf94021c8ed64212c8a270605
vhalvarez <alvarez.programmer@gmail.com>
2021-02-25 18:04:04 -0400

    Show orders on profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -80,8 +80,18 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Get logged in user order
+// @route   GET /api/orders/myorders
+// @access  Private
+const getMyOrders = asyncHandler(async (req, res) => {
+    const orders = await Order.find({user: req.user._id})
+
+    res.json(orders)
+})
+
 module.exports = {
     addOrderItems,
     getOrderById,
     updateOrderToPaid,
+    getMyOrders
 }
