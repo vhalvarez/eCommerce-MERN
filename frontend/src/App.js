de992b2d17995026cf4e0851722de125de7d7ac5
vhalvarez <alvarez.programmer@gmail.com>
2021-02-26 00:31:02 -0400

    Admin user list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
+import UserListScreen from './screens/UserListScreen'
 
 const App = () => {
     return (
@@ -22,7 +23,6 @@ const App = () => {
                 <Container>
                     <Route path='/' component={HomeScreen} exact />
                     <Route path='/product/:id' component={ProductScreen} />
-                    
                     <Route path='/login' component={LoginScreen} />
                     <Route path='/shipping' component={ShippingScreen} />
                     <Route path='/payment' component={PaymentScreen} />
@@ -31,6 +31,7 @@ const App = () => {
                     <Route path='/register' component={RegisterScreen} />
                     <Route path='/profile' component={ProfileScreen} />
                     <Route path='/cart/:id?' component={CartScreen} />
+                    <Route path='/admin/userList' component={UserListScreen} />
                 </Container>
             </main>
             <Footer />
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -14,8 +14,12 @@ import {
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL,
+    USER_LIST_RESET,
 } from '../constants/userConstans'
-import { ORDER_LIST_MY_RESET} from '../constants/orderConstants'
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -60,7 +64,7 @@ export const logout = () => (dispatch) => {
     dispatch({ type: USER_LOGOUT })
     dispatch({ type: USER_DETAILS_RESET })
     dispatch({ type: ORDER_LIST_MY_RESET })
-    // dispatch({ type: USER_LIST_RESET })
+    dispatch({ type: USER_LIST_RESET })
     document.location.href = '/login'
 }
 
@@ -164,6 +168,39 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: USER_UPDATE_PROFILE_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        })
+    }
+}
+
+export const listUsers = () => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_LIST_REQUEST,
+        })
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        }
+
+        const { data } = await axios.put(`/api/users`, config)
+
+        dispatch({
+            type: USER_LIST_SUCCESS,
+            payload: data,
+        })
+    } catch (error) {
+        dispatch({
+            type: USER_LIST_FAIL,
             payload:
                 error.response && error.response.data.message
                     ? error.response.data.message
